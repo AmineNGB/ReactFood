cc09a0f6c38ba1809408d006e1fe15d82e10864b
amineneghbel <amineneghbel@gmail.com>
2020-07-21 19:28:08 +0200

    Updates

---
+++ precache-manifest.a170107a8c79b9dea9b509cb158f409c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "815852b98cfd976880da9f88ecc5892e",
+    "url": "/ReactFood/static/media/epinard.815852b9.jpg"
+  },
+  {
+    "revision": "074e942fbac102453b0c",
+    "url": "/ReactFood/static/css/main.8c581080.chunk.css"
+  },
+  {
+    "revision": "ae120ab271ca4ef7945d",
+    "url": "/ReactFood/static/js/1.ae120ab2.chunk.js"
+  },
+  {
+    "revision": "299776f69d5e71e531fd",
+    "url": "/ReactFood/static/js/runtime~main.299776f6.js"
+  },
+  {
+    "revision": "b9a86c50cc3fe786eb584dd4dc784caf",
+    "url": "/ReactFood/static/media/default.b9a86c50.jpeg"
+  },
+  {
+    "revision": "4200b6b96b6c171e52c4a97d34a1174e",
+    "url": "/ReactFood/static/media/chocolat.4200b6b9.jpeg"
+  },
+  {
+    "revision": "074e942fbac102453b0c",
+    "url": "/ReactFood/static/js/main.074e942f.chunk.js"
+  },
+  {
+    "revision": "fca17319421519cbb9d73c85b4879566",
+    "url": "/ReactFood/static/media/hamburger.fca17319.jpeg"
+  },
+  {
+    "revision": "287d53ae5223bf74627baa68ecc5f5c1",
+    "url": "/ReactFood/static/media/pizza.287d53ae.jpeg"
+  },
+  {
+    "revision": "8324a6142c5fc9d0bdd0b453c162f9a1",
+    "url": "/ReactFood/static/media/saumon.8324a614.jpeg"
+  },
+  {
+    "revision": "96c2f7548bab68d96c489276dbc837e6",
+    "url": "/ReactFood/static/media/tartine.96c2f754.jpg"
+  },
+  {
+    "revision": "59a9832e19f7c0e6f0f0a132fe17d3e8",
+    "url": "/ReactFood/index.html"
+  }
+];
