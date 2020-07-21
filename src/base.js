e1dba37b17289523df9603f12ea121b5d2b6f14e
amineneghbel <amineneghbel@gmail.com>
2020-07-21 19:18:20 +0200

    firebase + facebook + reactFood done

--- src/base.js
+++ src/base.js
@@ -3,15 +3,13 @@ import firebase from 'firebase/app'
 import 'firebase/database'
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'xxx',
-  authDomain: 'xxx',
-  databaseURL: 'xxx'
+  apiKey: "AIzaSyD4gZdkaekxXs2Q8FGg4mZyfFi4wH-yjvY",
+  authDomain: "reactfood-a051c.firebaseapp.com",
+  databaseURL: "https://reactfood-a051c.firebaseio.com",
 })
 
 const base = Rebase.createClass(firebaseApp.database())
 
-// This is a named export
 export { firebaseApp }
 
-// this is a default export
 export default base
--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { Component, Fragment } from 'react'
 import { Redirect } from 'react-router-dom'
 
-class Connexion extends React.Component {
+import withPlaceholder from '../hoc/withPlaceholder'
+
+class Connexion extends Component {
   state = {
     pseudo: '',
     goToApp: false
@@ -23,22 +25,26 @@ class Connexion extends React.Component {
     }
 
     return (
-      <div className='connexionBox'>
-        <form className='connexion' onSubmit={this.goToApp} >
-          <h1>Ma Boîte à Recettes</h1>
-          <input
-            type='text'
-            value={this.state.pseudo}
-            onChange={this.handleChange}
-            placeholder='Nom du Chef'
-            pattern='[A-Za-z-]{1,}'
-            required />
-          <button type='submit'>GO</button>
-          <p>Pas de caractères spéciaux.</p>
-        </form>
-      </div>
+      <Fragment>
+        <div className='connexionBox'>
+          <form className='connexion' onSubmit={this.goToApp} >
+            <h1>Ma Boîte à Recettes</h1>
+            <input
+              type='text'
+              value={this.state.pseudo}
+              onChange={this.handleChange}
+              placeholder={this.props.placeholder}
+              pattern='[A-Za-z-]{1,}'
+              required />
+            <button type='submit'>GO</button>
+          </form>
+        </div>
+        <p>Pas de caractères spéciaux.</p>
+      </Fragment>
     )
   }
 }
 
-export default Connexion
+const WrappedComponent = withPlaceholder(Connexion)
+
+export default WrappedComponent
